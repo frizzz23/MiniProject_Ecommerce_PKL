2593f2959b8c57fe7a438f822fa7c5729e3cdd88
Hamzahxou <hamzahxou@gmail.com>
2024-12-17 13:08:32 +0700

    memperbaiki register

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -2,7 +2,6 @@
 @extends('layouts.admin')
 
 @section('main')
-
     <div class="container-fluid">
         <div class="container">
             <div class="card w-100">
@@ -21,13 +20,10 @@
                                         No
                                     </th>
                                     <th class="border-bottom-0">
-                                        Nama Pengguna
+                                        User
                                     </th>
                                     <th class="border-bottom-0">
                                         Produk
-                                    </th>
-                                    <th class="border-bottom-0">
-                                        Subtotal
                                     </th>
                                     <th class="border-bottom-0">
                                         Total
@@ -50,16 +46,15 @@
                                             {{ $order->user->name }}
                                         </td>
                                         <td class="border-bottom-0">
-
-                                            @foreach ($order->productOrders as $productOrder)
-                                                <span>{{ $productOrder->product->name_product }}</span><br>
-                                            @endforeach
-
-
-                                        </td>
-                                        <td class="border-bottom-0">
-                                            Rp.
-                                            {{ number_format($order->sub_total_amount, 2) }}
+                                            <ul class="list-disc">
+                                                @foreach ($order->productOrders as $productOrder)
+                                                    <li class="flex justify-between items-center">
+                                                        <span>{{ $productOrder->product->name_product }}</span>
+                                                        <span
+                                                            class="text-xs text-slate-700">x{{ $productOrder->quantity }}</span>
+                                                    </li>
+                                                @endforeach
+                                            </ul>
                                         </td>
                                         <td class="border-bottom-0">
                                             Rp.
@@ -72,6 +67,10 @@
                                             </div>
                                         </td>
                                         <td class="border-bottom-0">
+                                            <button type="button" class="btn btn-success btn-sm" data-bs-toggle="modal"
+                                                data-bs-target="#detailOrderModal{{ $order->id }}">
+                                                Detail
+                                            </button>
                                             <!-- Tombol Edit -->
                                             <button type="button" class="btn btn-warning btn-sm" data-bs-toggle="modal"
                                                 data-bs-target="#editOrderModal{{ $order->id }}">
@@ -90,6 +89,32 @@
                                             </form>
                                         </td>
                                     </tr>
+
+                                    <div class="modal fade" id="detailOrderModal{{ $order->id }}" tabindex="-1"
+                                        aria-labelledby="detailOrderModalLabel{{ $order->id }}" aria-hidden="true">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title" id="detailOrderModalLabel{{ $order->id }}">
+                                                        Detail Pesanan</h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                        aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <!-- Form inside modal -->
+
+                                                    <!-- Form fields for editing -->
+                                                    <div class="mb-3">
+
+                                                    </div>
+                                                    <div class="modal-footer">
+                                                        <button type="button" class="btn btn-secondary"
+                                                            data-bs-dismiss="modal">Kembali</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                     <div class="modal fade" id="editOrderModal{{ $order->id }}" tabindex="-1"
                                         aria-labelledby="editOrderModalLabel{{ $order->id }}" aria-hidden="true">
                                         <div class="modal-dialog">
@@ -101,6 +126,7 @@
                                                         aria-label="Close"></button>
                                                 </div>
                                                 <div class="modal-body">
+
                                                     <!-- Form inside modal -->
                                                     <form action="{{ route('admin.orders.update', $order->id) }}"
                                                         method="POST">
