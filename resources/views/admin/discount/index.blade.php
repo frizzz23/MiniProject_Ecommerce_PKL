fb14c0c5ccc20761ce3e3bcf8579ca47ab74e5bd
0IU5 <riopinayungan@github.com>
2024-12-24 08:08:22 +0700

    halaman admin

--- resources/views/admin/discount/index.blade.php
+++ resources/views/admin/discount/index.blade.php
@@ -29,35 +29,15 @@
                                     data-bs-target="#tambahmodal">
                                     + Tambah Vocher
                                 </button>
-                                <!-- Filter Kategori -->
-                                <form id="filterForm" action="{{ route('admin.products.index') }}" method="GET">
-                                    <div class="d-flex align-items-center ">
-                                        <select name="category_id"
-                                            class="bg-[#5d85fa] text-white border border-gray-600 rounded-lg py-2 px-3 w-full"
-                                            style="width: 200px;"
-                                            onchange="document.getElementById('filterForm').submit();">
-                                            <option value="">Semua Kategori</option>
-
-                                        </select>
-                                    </div>
-                                </form>
                             </div>
                         </div>
                         <form action="{{ route('admin.products.index') }}" method="GET">
-                            <div class="grid grid-cols-4 gap-4 text-white border-t border-gray-600 pt-4 mb-4">
-                                <div>
-                                    <select name="brand_id"
-                                        class="bg-[#5d85fa] text-white border border-gray-600 rounded-lg py-2 px-3 w-full"
-                                        onchange="this.form.submit()">
-                                        <option value="">Merek</option>
-
-                                    </select>
-                                </div>
+                            <div class="grid grid-cols-3 gap-3 text-white border-t border-gray-600 pt-4 mb-4">
                                 <div>
                                     <select name="price_product"
                                         class="bg-[#5d85fa] text-white border border-gray-600 rounded-lg py-2 px-3 w-full"
                                         onchange="this.form.submit()">
-                                        <option value="">Harga</option>
+                                        <option value="">Diskon</option>
                                         <option value="asc" {{ request('price_product') == 'asc' ? 'selected' : '' }}>
                                             Terendah
                                             ke Tertinggi</option>
@@ -70,21 +50,24 @@
                                     <select name="stock_product"
                                         class="bg-[#5d85fa] text-white border border-gray-600 rounded-lg py-2 px-3 w-full"
                                         onchange="this.form.submit()">
-                                        <option value="">Stok</option>
-                                        <option value="1" {{ request('stock_product') == '1' ? 'selected' : '' }}>
-                                            Ada
-                                        </option>
-                                        <option value="0" {{ request('stock_product') == '0' ? 'selected' : '' }}>
-                                            Habis
-                                        </option>
+                                        <option value="">Kuantitas</option>
+                                        <option value="asc" {{ request('price_product') == 'asc' ? 'selected' : '' }}>
+                                            Terbanyak ke Sedikit</option>
+                                        <option value="desc" {{ request('price_product') == 'desc' ? 'selected' : '' }}>
+                                            Sedikit ke terbanyak</option>
                                     </select>
                                 </div>
                                 <div>
                                     <select name="color_product"
                                         class="bg-[#5d85fa] text-white border border-gray-600 rounded-lg py-2 px-3 w-full"
                                         onchange="this.form.submit()">
-                                        <option value="">Warna</option>
-                                        <!-- Tambahkan opsi warna jika diperlukan -->
+                                        <option value="">Minimal Pembelian</option>
+                                        <option value="asc" {{ request('price_product') == 'asc' ? 'selected' : '' }}>
+                                            Terendah
+                                            ke Tertinggi</option>
+                                        <option value="desc" {{ request('price_product') == 'desc' ? 'selected' : '' }}>
+                                            Tertinggi
+                                            ke Terendah</option>
                                     </select>
                                 </div>
                             </div>
